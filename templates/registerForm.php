4d43bd959c401d1fc7806943f16f1842aadf3562
user.nale <loic.albert.denis@gmail.com>
2020-06-04 14:42:06 +0200

    change structure

--- templates/registerForm.php
+++ templates/registerForm.php
@@ -1,16 +1,8 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<?php include 'head.php' ?>
 
+
+<?php include "navbar.php" ?>
 <div class="container">
-    <div class="row">
-        <!-- <div class="col">
-            <h1 class="display-4">KEEP CALM & LOVE FOXES</h1>
-            <blockquote class="blockquote text-center">
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.</p>
-                <footer class="blockquote-footer">Maybe someone famous from <cite>Internet</cite></footer>
-            </blockquote>
-        </div> -->
-    </div>
     <div class="row">
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto my-5">
             <form class="form-signin" method="POST" action="?action=register">
